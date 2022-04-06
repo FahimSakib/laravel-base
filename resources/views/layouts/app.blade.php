36a49a81596dac593cb908f14a56fefcd7aba9d5
FahimSakib <fsakib991@gmail.com>
2022-04-06 14:46:57 +0600

    mastering and installing package initial

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,115 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from wieldy-html.g-axon.work/default/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 19 Aug 2020 08:12:55 GMT -->
+
 <head>
+    <!-- Meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Wieldy - A fully responsive, HTML5 based admin template">
+    <meta name="keywords"
+        content="Responsive, HTML5, admin theme, business, professional, jQuery, web design, CSS3, sass">
+    <!-- /meta tags -->
+    <title>Wieldy - Admin Dashboard</title>
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Site favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">
+    <!-- /site favicon -->
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Font Icon Styles -->
+    <link rel="stylesheet" href="../node_modules/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="../vendors/gaxon-icon/style.css">
+    <!-- /font icon Styles -->
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Perfect Scrollbar stylesheet -->
+    <link rel="stylesheet" href="../node_modules/perfect-scrollbar/css/perfect-scrollbar.css">
+    <!-- /perfect scrollbar stylesheet -->
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <!-- Load Styles -->
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/lite-style-1.min.css">
+    <!-- /load styles -->
+
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+<body class="dt-sidebar--fixed dt-header--fixed">
 
-                    </ul>
+    <!-- Loader -->
+    <div class="dt-loader-container">
+        <div class="dt-loader">
+            <svg class="circular" viewBox="25 25 50 50">
+                <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10">
+                </circle>
+            </svg>
+        </div>
+    </div>
+    <!-- /loader -->
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+    <!-- Root -->
+    <div class="dt-root">
+        <!-- Header -->
+        <header class="dt-header">
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+            <!-- Header container -->
+            @include('include.header')
+            <!-- /header container -->
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+        </header>
+        <!-- /header -->
+
+        <!-- Site Main -->
+        <main class="dt-main">
+            <!-- Sidebar -->
+            <x-sidebar/>
+            <!-- /sidebar -->
+
+            <!-- Site Content Wrapper -->
+            <div class="dt-content-wrapper">
+
+                <!-- Site Content -->
+                @yield('content')
+                <!-- /site content -->
+
+                <!-- Footer -->
+                @include('include.footer')
+                <!-- /footer -->
+
             </div>
-        </nav>
+            <!-- /site content wrapper -->
 
-        <main class="py-4">
-            @yield('content')
+            <!-- Theme Chooser -->
+            <div class="dt-customizer-toggle">
+                <a href="javascript:void(0)" data-toggle="customizer"> <i class="icon icon-spin icon-setting"></i> </a>
+            </div>
+            <!-- /theme chooser -->
+
+            <!-- Customizer Sidebar -->
+            <x-right-sidebar/>
+            <!-- /customizer sidebar -->
+
         </main>
     </div>
+    <!-- /root -->
+
+    <!-- Optional JavaScript -->
+    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="../node_modules/moment/moment.js"></script>
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Perfect Scrollbar jQuery -->
+    <script src="../node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js"></script>
+    <!-- /perfect scrollbar jQuery -->
+
+    <!-- masonry script -->
+    <script src="../node_modules/masonry-layout/dist/masonry.pkgd.min.js"></script>
+    <script src="../node_modules/sweetalert2/dist/sweetalert2.js"></script>
+
+    <script src="../node_modules/chart.js/dist/Chart.min.js"></script>
+    <script src="assets/js/script.js"></script>
+    <script src="assets/js/custom/charts/dashboard-crypto.js"></script>
 </body>
+
+<!-- Mirrored from wieldy-html.g-axon.work/default/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 19 Aug 2020 08:14:49 GMT -->
+
 </html>
