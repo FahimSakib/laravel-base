e144319beb961219cf4a642b75892b6c01e045a6
FahimSakib <fsakib991@gmail.com>
2022-04-22 01:55:46 +0600

    index customizing initial

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController;
 
-class MenuController extends Controller
+class MenuController extends BaseController
 {
-    //
+    public function index()
+    {
+        $this->setPageData('Menu','Menu','fas fa-th-list');
+        return view('menu.index');
+    }
+
+    public function getDatatableData(Request $request)
+    {
+        if($request->ajax()){
+            $output = '';
+        }else{
+            $output = ['status' => 'error', 'message' => 'Unauthorized action blocked!'];
+        }
+
+        return response()->json($output);
+    }
 }
