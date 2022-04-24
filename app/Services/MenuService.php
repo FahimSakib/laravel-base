4ad62476a3b7ca24916f0e173197dbc021d864bb
FahimSakib <fsakib991@gmail.com>
2022-04-24 14:55:23 +0600

    minor design change

--- app/Services/MenuService.php
+++ app/Services/MenuService.php
@@ -40,8 +40,8 @@ class MenuService extends BaseService{
                 $action .= ' <a class="dropdown-item delete_data"  data-id="' . $value->id . '" data-name="' . $value->menu_name . '"><i class="fas fa-trash text-danger"></i> Delete</a>';
 
                 $btngroup = '<div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-th-list"></i>
+                <button class="btn btn-secondary dropdown-toggle text-white" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-bars-staggered text-white"></i>
                 </button>
                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                 ' . $action . '
