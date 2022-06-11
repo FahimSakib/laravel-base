e8cbc2426716794fff6eea108bda7f9549f14b78
FahimSakib <fsakib991@gmail.com>
2022-06-11 12:46:00 +0600

    user permission

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -47,9 +47,15 @@ class UserService extends BaseService{
             foreach ($list as $value) {
                 $no++;
                 $action = '';
-                $action .= ' <a class="dropdown-item edit_data" data-id="' . $value->id . '"><i class="fas fa-edit text-primary"></i> Edit</a>';
-                $action .= ' <a class="dropdown-item view_data"  data-id="' . $value->id . '"><i class="fas fa-eye text-warning"></i> View</a>';
-                $action .= ' <a class="dropdown-item delete_data"  data-id="' . $value->id . '" data-name="' . $value->name . '"><i class="fas fa-trash text-danger"></i> Delete</a>';
+                if(permission('user-edit')){
+                    $action .= ' <a class="dropdown-item edit_data" data-id="' . $value->id . '"><i class="fas fa-edit text-primary"></i> Edit</a>';
+                }
+                if(permission('user-view')){
+                    $action .= ' <a class="dropdown-item view_data"  data-id="' . $value->id . '"><i class="fas fa-eye text-warning"></i> View</a>';
+                }
+                if(permission('user-delete')){
+                    $action .= ' <a class="dropdown-item delete_data"  data-id="' . $value->id . '" data-name="' . $value->name . '"><i class="fas fa-trash text-danger"></i> Delete</a>';
+                }
                 $btngroup = '<div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle text-white" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fas fa-bars-staggered text-white"></i>
@@ -60,11 +66,13 @@ class UserService extends BaseService{
               </div>';
 
                 $row = [];
-                $row[] = '<div class="custom-control custom-checkbox">
-                <input type="checkbox" value="'.$value->id.'"
-                class="custom-control-input select_data" onchange="select_single_item('.$value->id.')" id="checkbox'.$value->id.'">
-                <label class="custom-control-label" for="checkbox'.$value->id.'"></label>
-              </div>';
+                if(permission('user-bulk-delete')){
+                    $row[] = '<div class="custom-control custom-checkbox">
+                    <input type="checkbox" value="'.$value->id.'"
+                    class="custom-control-input select_data" onchange="select_single_item('.$value->id.')" id="checkbox'.$value->id.'">
+                    <label class="custom-control-label" for="checkbox'.$value->id.'"></label>
+                    </div>';
+                }
               $row[] = $no;
               $row[] = $this->avatar($value);
               $row[] = $value->name;
