397b72747097f722d7000e6832275adb65e5bf83
FahimSakib <fsakib991@gmail.com>
2022-06-12 10:56:17 +0600

    trait:uploadable changed

--- app/Traits/UploadAble.php
+++ app/Traits/UploadAble.php
@@ -5,50 +5,37 @@ namespace App\Traits;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 
-/**
- * Trait UploadAble
- * @package App\Traits
- */
-trait UploadAble
-{
-    /**
+trait Uploadable{
+
+    /************************
      * * Upload File Method * *
-     * @param UploadedFile $file
-     * @param null $folder
-     * @param null $filename
-     * @param null $disk
-     * @return false|string
-     */
-    public function upload_file(UploadedFile $file, $folder = null,  $file_name = null, $disk = 'public')
-    {
-        if (!Storage::directories($disk.'/'.$folder)) {
-			Storage::makeDirectory($disk.'/'.$folder,0777, true); //if directory not exist then make the directory
+     * ? @params UploadFile $file 
+     * ? @params $folder
+     * ? @params $file_name
+     * ? @params $disk 
+     * ! return $fileNameToStore
+     * *********************/
+
+    public function upload_file(UploadedFile $file,$folder=null,$file_name=null,$disk='public'){
+
+        if(!Storage::directories($disk.'/'.$folder)){
+            Storage::makeDirectory($disk.'/'.$folder,0777,true);
         }
 
-        $filenameWithExt = $file->getClientOriginalName(); // Get filename with extension like index.jpg
-        $filename        = pathinfo($filenameWithExt, PATHINFO_FILENAME); // Get just filename  like index          
-        $extension       = $file->getClientOriginalExtension(); // Get just extension like .jpg
+        $fileNameWithExtension = $file->getClientOriginalName();
+        $fileName              = pathinfo($fileNameWithExtension,PATHINFO_FILENAME);
+        $extension             = $file->getClientOriginalExtension();
+        $fileNameToStore       = !is_null($file_name) ? $file_name.'.'.$extension : $fileName.'_'.uniqid().'.'.$extension;
 
-        $fileNameToStore = !is_null($file_name) ? 
-        str_replace(' ', '-', $file_name).'.'.$extension : 
-        str_replace(' ', '-', $filename).'-'.rand(111111,999999).'.'.$extension; //Filename to store  like index1545gfh5465.jpg                
-        $file->storeAs($folder,$fileNameToStore,$disk); //store file in targetted folder 
+        $file->storeAs($folder,$fileNameToStore,$disk);
+
         return $fileNameToStore;
     }
 
-    /**
-     * ! Delete File Method ! 
-     * @param string $filename
-     * @param string $folder
-     * @param string $disk
-     * @return true|false
-     */
-    public function delete_file($filename,$folder,$disk = 'public')
-    {
-        if(Storage::exists($disk.'/'.$folder.$filename))
-        {
-            Storage::disk($disk)->delete($folder.$filename);
-            return TRUE;
+    public function delete_file($file_name,$folder,$disk='public'){
+        if(Storage::exists($disk.'/'.$folder.'/'.$file_name)){
+            Storage::disk($disk)->delete($folder.'/'.$file_name);
+            return true;
         }
         return false;
     }
