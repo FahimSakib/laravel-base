4e8adf1813882948568ae496970e2f7cff0ca139
FahimSakib <fsakib991@gmail.com>
2022-06-19 17:43:31 +0600

    setting/right side bar hided

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,13 +74,13 @@
             <!-- /site content wrapper -->
 
             <!-- Theme Chooser -->
-            <div class="dt-customizer-toggle">
+            {{-- <div class="dt-customizer-toggle">
                 <a href="javascript:void(0)" data-toggle="customizer"> <i class="icon icon-spin icon-setting"></i> </a>
-            </div>
+            </div> --}}
             <!-- /theme chooser -->
 
             <!-- Customizer Sidebar -->
-            <x-right-sidebar />
+            {{-- <x-right-sidebar /> --}}
             <!-- /customizer sidebar -->
 
         </main>
